191f293eed05b1a6e655f87cea6e56ccc84d73c2
synghsh <syn.ghsh.2020@gmail.com>
2024-06-26 00:25:30 +0530

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Navbar from '@/scenes/navbar';
+import { useState } from 'react';
+import { SelectedPage } from './shared/types';
+
+
 
 function App() {
-
+const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
   return (
       <div className="app bg-gray-50">
-        <Navbar/>
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
         </div>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,34 @@
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+
 interface Props {
-    
+    selectedPage: SelectedPage
+    setSelectedPage: (value: SelectedPage)=>void
 }
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage,setSelectedPage}: Props) => {
+    const flexBetween = "flex items-center justify-between";
     return (
-        <div>
-            Navbar
+        <div className={`${flexBetween} fixed top-0 w-full py-6`}>
+            <div className={`${flexBetween} mx-auto w-5/6 `}>
+                <div className={`${flexBetween} w-full gap-16`}>
+                    <img alt="logo" src={Logo} />
+                    <div className={`${flexBetween} w-full`}>
+                        <div className={`${flexBetween} gap-8 text-sm`}>
+                            <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Connect Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        </div>
+                    </div>
+                    <div className={`${flexBetween} gap-8 `}>
+                    <p>Sign In</p>
+                    <button>Become a Member</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
